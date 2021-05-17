03419c09b0d62a594916ba22a406b8902096b48e
t.babparn <tp.babparn@gmail.com>
2021-05-17 14:29:04 +0700

    first commit

---
+++ kus.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    username: 'admin',
+    email: 'admin@domain.com',
+    firstName: 'Admin',
+    lastName: 'Admin',
+    node_id: 'http://kong:8081',
+    admin: true,
+    active: true,
+    password: 'admin1234',
+  },
+];
